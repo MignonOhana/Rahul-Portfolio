ef05d4a54689c003d01ea1eefa9008caaa2eb87a
Abhinav Singh <83336285+abhinavsinghring@users.noreply.github.com>
2024-07-09 01:05:20 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/Rahul-Portfolio",
-    assetPrefix: '/Rahul-Portfolio/',
-    images: {
-        unoptimized: true, // Disable Image Optimization for static export
-    },
-    reactStrictMode: true,
 };
 
 export default nextConfig;
