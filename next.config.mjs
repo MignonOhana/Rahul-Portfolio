fe563fbe8ebb839a80335f653b92190f92698a12
MignonOhana <rahulsinghwillian@gmail.com>
2024-07-09 00:10:58 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   
+    basePath: "/Rahul-Portfolio",
+    assetPrefix: '/your-repo-name/',
+    images: {
+        unoptimized: true, // Disable Image Optimization for static export
+    },
 };
 
 export default nextConfig;
